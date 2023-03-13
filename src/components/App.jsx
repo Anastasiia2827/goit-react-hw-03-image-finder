598dc06648b675d62fb742c0dcc31d9b2c3f61fc
Anastasiia2827 <nasty.bond2016@gmail.com>
2023-03-13 17:41:31 +0200

    add folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
+import React, {Component} from "react";
+
+export class App extends Component  {
+  state = {
+    query: '',
+    page: 1,
+    images: [],
+    isLoading: false,
+    showModal: false,
+    currentImageUrl: null,
+    showLoadMoreBtn: false,
+  };
+
+
+render(){
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+     <p>hjhkhk</p>
     </div>
   );
 };
+}
