eba0082fe226f433d3d99244a186c2e5248413a6
Anastasiia2827 <nasty.bond2016@gmail.com>
2023-03-16 20:02:17 +0200

    js and styled

--- src/components/PixabayApi.js
+++ src/components/PixabayApi.js
@@ -2,12 +2,12 @@ import axios from "axios";
 const API_KEY = '33046480-b67f65ece8761789fa0ed8218';
 const BASE_URL = 'https://pixabay.com/api/';
 
-async function fetchImages(query, page=1) {
+async function fetchImages(query, page) {
     try {
         return await axios.get(BASE_URL, {
             params: {
                 key: API_KEY,
-                q: query,
+                q:  query ,
                 image_type: 'photo',
                 orientation: 'horizontal',
                 safesearch: true,
